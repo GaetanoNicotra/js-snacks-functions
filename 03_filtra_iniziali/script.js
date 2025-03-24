19d2cadb5746d7d91b9105d69972802d189d531a
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-24 18:46:34 +0100

    [UPD] BONUS: trasfomata la funzione dello snack 3 in funzione FRECCIA

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,7 +5,25 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
-function checkLetter(arr) {
+/*function checkLetter(arr) {
+    let letteraUtente = prompt("Inserisci la lettera").toUpperCase();
+    console.log("Hai scelto la lettera:" + " " + letteraUtente)
+    let array2 = [];
+
+    for (i = 0; i < arr.length; i++) {
+
+        let lettera = arr[i];
+
+        if (letteraUtente === lettera[0]) {
+            array2.push(lettera);
+        }
+    }
+    console.log(array2)
+}*/
+
+
+// ******** TRASFORMAZIONE IN FUNZIONE FRECCIA ********
+const checkLetter = (arr) => {
     let letteraUtente = prompt("Inserisci la lettera").toUpperCase();
     console.log("Hai scelto la lettera:" + " " + letteraUtente)
     let array2 = [];
@@ -20,6 +38,7 @@ function checkLetter(arr) {
     }
     console.log(array2)
 }
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
