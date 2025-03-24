122dbc3068bc7f0d51d01e1d699100c30e877e58
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-24 17:32:55 +0100

    [UPD] Terzo snack completato

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -5,9 +5,24 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function checkLetter(arr) {
+    let letteraUtente = prompt("Inserisci la lettera");
+    console.log("Hai scelto la lettera:" + " " + letteraUtente)
+    let array2 = [];
+
+    for (i = 0; i < arr.length; i++) {
+        
+        let lettera = arr[i];
+
+        if (letteraUtente === lettera[0]) {
+            array2.push(lettera);
+        }
+    }
+    console.log(array2)
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+checkLetter(names);
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
