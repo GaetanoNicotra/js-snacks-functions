5d9d15e86b8c93ff4a13b95b4f398d5c2c932970
GaetanoNicotra <nicotragaetano10@gmail.com>
2025-03-24 20:46:52 +0100

    [UPD] Bonus 5 terminato

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,32 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const myName = 'Mario';
 
 
 // Dichiara la funzione qui.
 
+const helloUser = (nameUser) =>{
+    let ora = new Date().getHours();
 
+    let salutoOutput;
+
+    if( ora >= 6 && ora < 13){
+      
+   salutoOutput = "Buongiorno"
+    }
+    else if(ora >= 13 && ora < 17){
+
+   salutoOutput = "Buon pomeriggio"
+   }
+   else{
+    salutoOutput = "Buona sera"
+   }
+
+   return `${salutoOutput}, ${nameUser}`;
+}
 // Invoca la funzione qui e stampa il risultato in console
 
 
-
+console.log(helloUser (myName))
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
